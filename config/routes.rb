74c0ef990a50984fc6d7ce7a7e95bd3c9fe7329a
Ana <anatdona@gmail.com>
2021-01-05 14:28:49 -0300

    Blog second commit, Heroku, please accept this offering, cuz you've made me religious

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index'
+  get 'posts/dashboard'
+  get 'posts/create'
+  
+  post '/posts/create', to: 'posts#create', as:'posts'
+  
+  root 'posts#index'
+
 end
